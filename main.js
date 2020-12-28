ef70837870a32d18cc3ee5fd21384fdb1ab79086
ZSEN <huzhushan@itcast.cn>
2020-12-28 16:35:24 +0800

    init

---
+++ main.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import App from './App'
+import { $http } from '@escook/request-miniprogram'
+// 导入 store 的实例对象
+import store from './store'
+
+App.mpType = 'app'
+
+uni.$http = $http
+// 配置请求根路径
+$http.baseUrl = 'https://www.uinav.com'
+
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+  
+  // 判断请求的是否为有权限的 API 接口
+  if (options.url.indexOf('/my/') !== -1) {
+    // 为请求头添加身份认证字段
+    options.header = {
+      // 字段的值可以直接从 vuex 中进行获取
+      Authorization: store.state.m_user.token,
+    }
+  }
+}
+
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
+
+Vue.config.productionTip = false
+
+// 封装的展示消息提示的方法
+uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
+
+
+const app = new Vue({
+	...App,
+  store,
+})
+app.$mount()
---
+++ pages/my/my.vue
@@ -0,0 +1,36 @@
+<template>
+  <view class="my-container">
+    <!-- 用户未登录时，显示登录组件 -->
+    <my-login v-if="!token"></my-login>
+
+    <!-- 用户登录后，显示用户信息组件 -->
+    <my-userinfo></my-userinfo>
+  </view>
+</template>
+
+<script>
+  import {
+    mapState
+  } from 'vuex'
+  // 导入自己封装的 mixin 模块
+  import badgeMix from '@/mixins/tabbar-badge.js'
+  export default {
+    // 将 badgeMix 混入到当前的页面中进行使用
+    mixins: [badgeMix],
+    data() {
+      return {
+
+      };
+    },
+    computed: {
+      ...mapState('m_user', ['token']),
+    }
+  }
+</script>
+
+<style lang="scss">
+  page,
+  .my-container {
+    height: 100%;
+  }
+</style>
